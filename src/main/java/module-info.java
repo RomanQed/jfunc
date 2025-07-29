31d3c1b584ee9e77771e5cea434e3bd35fa3bb63
RomanQed <gbakaldin@gmail.com>
2025-07-29 21:10:41 +0300

    Add javadoc for module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,7 @@
-open module com.github.romanqed.jfunc {
+/**
+ * Provides functional interfaces that support {@code checked} exceptions and utility types
+ * for composing, lazily evaluating, and wrapping functional calls.
+ */
+module com.github.romanqed.jfunc {
     exports com.github.romanqed.jfunc;
 }
