2875856b7c41f71d3b679151e39fa8489df68555
Roman Bakaldin <gbakaldin@gmail.com>
2025-08-03 21:54:11 +0300

    Fix missing javadoc elements

--- juni/src/main/java/com/github/romanqed/juni/UniRunnable0.java
+++ juni/src/main/java/com/github/romanqed/juni/UniRunnable0.java
@@ -14,7 +14,10 @@ import java.util.concurrent.ExecutionException;
 public interface UniRunnable0 extends Uni, Runnable0, AsyncRunnable0 {
 
     /**
-     * Wraps a synchronous runnable into a {@code UniRunnable0}.
+     * Wraps a synchronous runnable into a {@link UniRunnable0}.
+     *
+     * @param func the runnable to wrap
+     * @return a unified runnable that executes synchronously
      */
     static UniRunnable0 of(Runnable0 func) {
         return new UniRunnable0() {
@@ -43,7 +46,10 @@ public interface UniRunnable0 extends Uni, Runnable0, AsyncRunnable0 {
     }
 
     /**
-     * Wraps an asynchronous runnable into a {@code UniRunnable0}.
+     * Wraps an asynchronous runnable into a {@link UniRunnable0}.
+     *
+     * @param func the runnable to wrap
+     * @return a unified runnable that executes asynchronously
      */
     static UniRunnable0 of(AsyncRunnable0 func) {
         return new UniRunnable0() {
